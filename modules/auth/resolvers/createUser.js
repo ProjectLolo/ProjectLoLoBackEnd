fb9654001a966ec51ea84c78ee677614104134aa
Andre2610 <dre2610@gmail.com>
2020-11-03 13:55:15 +0100

    created jwt functions middleware

--- modules/auth/resolvers/createUser.js
+++ modules/auth/resolvers/createUser.js
@@ -1,15 +1,16 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { SALT_ROUNDS } = require('../../../config/constants')
 
 const User = require("../../../models/User");
 
-const createUser = async (args) => {
+const createUser = async (_, userInput) => {
     try {
-      const existingUser = await User.findOne({ email: args.userInput.email });
+      const existingUser = await User.findOne({ email: userInput.email });
       if (existingUser) {
         throw new Error("User exists already.");
       }
-      const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+      const hashedPassword = await bcrypt.hash(args.userInput.password, SALT_ROUNDS);
 
       const user = new User({
         firstName: args.userInput.firstName,
