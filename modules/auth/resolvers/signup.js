ab651cd41948cb2d70a3b2c7914f9be84c2fc436
Andre2610 <dre2610@gmail.com>
2020-11-03 14:05:34 +0100

    resolving conflicts

--- modules/auth/resolvers/signup.js
+++ modules/auth/resolvers/signup.js
@@ -4,20 +4,27 @@ const { SALT_ROUNDS } = require('../../../config/constants')
 
 const User = require("../../../models/User");
 
-const createUser = async (_, userInput) => {
+const createUser = async (_, { 
+  signupInput: { 
+    firstName, 
+    lastName, 
+    nickName, 
+    password, 
+    email}
+}) => {
     try {
       const existingUser = await User.findOne({ email: userInput.email });
       if (existingUser) {
         throw new Error("User exists already.");
       }
-      const hashedPassword = await bcrypt.hash(args.userInput.password, SALT_ROUNDS);
+      const hashedPassword = await bcrypt.hash(userInput.password, SALT_ROUNDS);
 
       const user = new User({
-        firstName: args.userInput.firstName,
-        lastName: args.userInput.lastName,
-        nickName: args.userInput.nickName,
+        firstName: userInput.firstName,
+        lastName: userInput.lastName,
+        nickName: userInput.nickName,
         password: hashedPassword,
-        email: args.userInput.email,
+        email: userInput.email,
       });
 
       const result = await user.save();
