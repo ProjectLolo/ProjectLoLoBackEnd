6ad49c5d9fbad037d6f79854e3bdf8b1c272f80a
Atieh Hamidi <hamidiatiye@gmail.com>
2020-11-04 11:39:48 +0100

    added typeDefs for familyMembers

---
+++ modules/familyMembers/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  extend type Mutation {
+    addMember(memberInput: MemberInput): FamilyMember
+  }
+
+  type FamilyMember {
+    id: ID!
+    kidId: String!
+    userId: String!
+    relation: String!
+    notification: String!
+  }
+
+  input MemberInput {
+    kidId: String!
+    userId: String!
+    relation: String!
+    notification: String!
+  }
+`;
+
+const resolvers = require("./resolvers");
+
+module.exports = {
+  typeDefs: [typeDefs],
+  resolvers,
+};
