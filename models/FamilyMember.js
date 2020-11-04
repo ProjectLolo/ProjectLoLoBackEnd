c77f7951bc561f92ddac06f5a5745dfe76a31ecc
Atieh Hamidi <hamidiatiye@gmail.com>
2020-11-04 11:35:56 +0100

    changed the familyMember because enum dosen't work in mangoose

--- models/FamilyMember.js
+++ models/FamilyMember.js
@@ -1,34 +1,44 @@
-const { model, Schema } = require('mongoose')
+const { model, Schema } = require("mongoose");
 
 const familyMemberSchema = new Schema({
-    relation: {
-        type: String,
-        enum : ['Father','Mother', 'Grand Father', 'Grand-Mother','Aunt','Uncle', 'Gaurdian'],
-        default: 'Gaurdian',
-    },
-    notification:{
-        type: Number,
-        enum : [1, 2, 3],
-        default: 1,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    modifiedAt: {
-        type: Date,
-        default: Date.now,
-      },
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required:true,
-    },
-    kidId: {
-        type: Schema.Types.ObjectId,
-        ref:'Kid',
-        required:true,
-    },
-})
+  relation: {
+    type: String,
+    enum: [
+      "Father",
+      "Mother",
+      "Grand Father",
+      "Grand-Mother",
+      "Aunt",
+      "Uncle",
+      "Gaurdian",
+    ],
+    default: "Gaurdian",
+  },
+  notification: {
+    type: String,
+    enum: ["1", "2", "3"],
+    default: 1,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  modifiedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  userId: {
+    type: String,
+    // type: Schema.Types.ObjectId,
+    // ref: 'User',
+    // required:true,
+  },
+  kidId: {
+    type: String,
+    // type: Schema.Types.ObjectId,
+    // ref:'Kid',
+    // required:true,
+  },
+});
 
-module.exports = model('FamilyMember', familyMemberSchema)
+module.exports = model("FamilyMember", familyMemberSchema);
