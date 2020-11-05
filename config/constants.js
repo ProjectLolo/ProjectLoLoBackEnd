2bfb2894b07cec4f44ee09239ece113538da5aee
Atieh Hamidi <hamidiatiye@gmail.com>
2020-11-05 11:10:37 +0100

    added some changes for login mutation

--- config/constants.js
+++ config/constants.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 module.exports = {
-    SALT_ROUNDS: 10,
-    PORT: process.env.PORT || 5000,
-    BACKEND_API: process.env.BACKEND_API || "http://localhost:5000",
-    MONGODB: process.env.MONGODB,
-    SECRET_KEY: process.env.JWT_SECRET || "e9rp^&^*&@9sejg)DSUA)jpfds8394jdsfn,m"
+  SALT_ROUNDS: 10,
+  PORT: process.env.PORT || 5000,
+  BACKEND_API: process.env.BACKEND_API || "http://localhost:5000",
+  MONGODB: process.env.MONGODB,
+  SECRET_KEY: process.env.SECRET_KEY || "e9rp^&^*&@9sejg)DSUA)jpfds8394jdsfn,m",
 };
