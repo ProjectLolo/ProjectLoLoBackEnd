c19b5473f2f8046abe9456068f61702c17722295
Atieh Hamidi <hamidiatiye@gmail.com>
2020-11-06 15:07:14 +0100

    removed Media model and add new properties to the lovabank model

--- models/LoveBank.js
+++ models/LoveBank.js
@@ -2,16 +2,45 @@ const { model, Schema } = require("mongoose");
 
 const loveBankSchema = new Schema(
   {
+    title: {
+      type: Text,
+      required: true,
+    },
+    url: {
+      type: Text,
+      required: true,
+    },
+    description: {
+      type: Text,
+    },
+    category: {
+      type: String,
+      enum: ["audio", "video", "other"],
+      default: "video",
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     kidId: {
       type: Schema.Types.ObjectId,
       ref: "Kid",
       required: true,
     },
-    mediaId: {
-      type: Schema.Types.ObjectId,
-      ref: "Media",
-      required: true,
-    },
+    comments: [
+      {
+        userId: { type: Schema.Types.ObjectId },
+        comment: String,
+        createdAt: String,
+      },
+    ],
+    likes: [
+      {
+        userId: { type: Schema.Types.ObjectId },
+        createdAt: String,
+      },
+    ],
   },
   { timestamps: true }
 );
