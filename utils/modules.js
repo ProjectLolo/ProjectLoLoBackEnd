dca8891a2b45809ad8a5f4c0f7e219c0f358b20b
Jenni <jenni.svensson@live.se>
2020-11-12 23:37:55 +0100

    Deleted stuff we are not using

--- utils/modules.js
+++ utils/modules.js
@@ -1,45 +1,31 @@
-const { gql, makeExecutableSchema } = require('apollo-server')
-const deepmerge = require('deepmerge')
-
-const directives = require('../directives')
-const scalars = require('../scalars')
+const { gql, makeExecutableSchema } = require("apollo-server");
+const deepmerge = require("deepmerge");
+const scalars = require("../scalars");
 
 const globalTypeDefs = gql`
   type Query
   type Mutation
-`
+`;
 
-const makeExecutableSchemaFromModules = ({
-  modules
-}) => {
-  let typeDefs = [
-    globalTypeDefs,
-    ...scalars.typeDefs,
-    ...directives.typeDefs
-  ]
+const makeExecutableSchemaFromModules = ({ modules }) => {
+  let typeDefs = [globalTypeDefs, ...scalars.typeDefs];
 
   let resolvers = {
-    ...scalars.resolvers
-  }
+    ...scalars.resolvers,
+  };
 
-  modules.forEach(module => {
-    typeDefs = [
-      ...typeDefs,
-      ...module.typeDefs
-    ]
+  modules.forEach((module) => {
+    typeDefs = [...typeDefs, ...module.typeDefs];
 
-    resolvers = deepmerge(resolvers, module.resolvers)
-  })
+    resolvers = deepmerge(resolvers, module.resolvers);
+  });
 
   return makeExecutableSchema({
     typeDefs,
     resolvers,
-    schemaDirectives: {
-      ...directives.schemaDirectives
-    }
-  })
-}
+  });
+};
 
 module.exports = {
-  makeExecutableSchemaFromModules
-}
+  makeExecutableSchemaFromModules,
+};
