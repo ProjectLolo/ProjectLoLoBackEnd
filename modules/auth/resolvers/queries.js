1f37f4c20b72e7250e7ffc12650a93538965c911
Atieh Hamidi <hamidiatiye@gmail.com>
2020-11-13 10:14:16 +0100

    added query for getting user by Id

--- modules/auth/resolvers/queries.js
+++ modules/auth/resolvers/queries.js
@@ -1,3 +1,14 @@
 const User = require("../../../models/User");
+const checkAuth = require("../../../utils/check-auth");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
-module.exports = {};
+const findUserById = async (_, { id }, context) => {
+  const { userId } = checkAuth(context);
+  const user = await User.findById(id);
+  if (user) {
+    return user;
+  } else {
+    throw new UserInputError("User doesn't exist");
+  }
+};
+module.exports = { findUserById };
