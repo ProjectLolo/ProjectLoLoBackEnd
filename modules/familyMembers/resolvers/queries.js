5d2932d1591f902a97f0e5ba1f41790d2e808f82
Jenni <jenni.svensson@live.se>
2020-11-17 10:27:30 +0100

    Got rid of query

--- modules/familyMembers/resolvers/queries.js
+++ modules/familyMembers/resolvers/queries.js
@@ -1,15 +1,3 @@
 const FamilyMember = require("../../../models/FamilyMember");
 
-const findKidBelongsToMember = async (_, userId) => {
-  const member = await FamilyMember.find(userId).populate("kid");
-
-  console.log("test", member);
-
-  const result = member.map((kid) => {
-    return kid.kid;
-  });
-
-  return result;
-};
-
-module.exports = { findKidBelongsToMember };
+module.exports = {};
